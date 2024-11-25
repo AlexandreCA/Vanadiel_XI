c9468d5979286a058f5e398346194687d8686beb
Fox_mulder <alexandre.mate@hotmail.com>
2024-11-25 15:38:30 -0500

    correctif

---
+++ scripts/zones/Bastok_Mines/npcs/Gavoroi.lua
@@ -0,0 +1,23 @@
+-----------------------------------
+-- Area: Bastok Mines
+--  NPC: Gavoroi
+-----------------------------------
+local ID = zones[xi.zone.BASTOK_MINES]
+-----------------------------------
+---@type TNpcEntity
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+    player:startEvent(552)
+end
+
+entity.onEventUpdate = function(player, csid, option, npc)
+end
+
+entity.onEventFinish = function(player, csid, option, npc)
+end
+
+return entity
