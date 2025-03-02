0df83ae2170cc56c9d85c9f2572f367499ecfe49
Fox_mulder <alexandre.mate@hotmail.com>
2025-03-02 02:05:40 -0500

    update du 2025-03-02

--- scripts/zones/East_Sarutabaruta/IDs.lua
+++ scripts/zones/East_Sarutabaruta/IDs.lua
@@ -35,6 +35,7 @@ zones[xi.zone.EAST_SARUTABARUTA] =
         ALREADY_POSSESS_TEMP          = 7570, -- You already possess that temporary item.
         NO_COMBINATION                = 7575, -- You were unable to enter a combination.
         UNITY_WANTED_BATTLE_INTERACT  = 7637, -- Those who have accepted % must pay # Unity accolades to participate. The content for this Wanted battle is #. [Ready to begin?/You do not have the appropriate object set, so your rewards will be limited.]
+		TIME_ELAPSED                  = 7722, -- Time elapsed: <number> [hour/hours] (Vana'diel time) <number> [minute/minutes] and <number> [second/seconds] (Earth time)																																								  
         REGIME_REGISTERED             = 9936, -- New training regime registered!
     },
     mob =
