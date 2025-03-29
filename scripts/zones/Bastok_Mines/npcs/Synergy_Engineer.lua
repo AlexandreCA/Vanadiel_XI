634870dcee0bf3e401b8e9fef04ff9a30e011a07
Fox_mulder <alexandre.mate@protonmail.com>
2025-03-29 13:35:18 -0400

    Create Synergy_Engineer.lua

---
+++ scripts/zones/Bastok_Mines/npcs/Synergy_Engineer.lua
@@ -0,0 +1,23 @@
+-----------------------------------
+-- Area: Bastok Mines
+--  NPC: Synergy Engineer
+-- !pos -43.2409 0.000 -64.1494
+-----------------------------------
+---@type TNpcEntity
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+	player:startEvent(11001) -- Déclenche l'événement 11001
+end
+
+entity.onEventUpdate = function(player, csid, option, npc)
+end
+
+entity.onEventFinish = function(player, csid, option, npc)
+    xi.synergy.onEventFinish(player, csid, option, npc)
+end
+
+return entity
