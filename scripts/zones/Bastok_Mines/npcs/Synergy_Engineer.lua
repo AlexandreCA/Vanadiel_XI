fbe3785fb1f7e56815e4452f36665e05056edd61
Fox_mulder <alexandre.mate@protonmail.com>
2025-03-30 19:08:17 -0400

    Update Synergy_Engineer.lua

--- scripts/zones/Bastok_Mines/npcs/Synergy_Engineer.lua
+++ scripts/zones/Bastok_Mines/npcs/Synergy_Engineer.lua
@@ -11,15 +11,25 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    player:printToPlayer("Debug: Starting event 11001 with params 1, 0, 5, 15498, 15499, 15500, 15501, 0")
-    player:startEvent(11001, 1, 0, 5, 15498, 15499, 15500, 15501, 0)
+    if player:hasKeyItem(xi.keyItem.SYNERGY_CRUCIBLE) then
+        player:printToPlayer("Debug: Player has SYNERGY_CRUCIBLE - Starting event 11001 with params 1, 0, 5, 15498, 15499, 15500, 15501, 0")
+        player:startEvent(11001, 1, 0, 5, 15498, 15499, 15500, 15501, 0)
+    else
+        player:printToPlayer("Debug: Player does NOT have SYNERGY_CRUCIBLE - Starting event 11001 with params 1, 0, 0, 0, 0, 0, 0, 0")
+        player:startEvent(11001, 1, 0, 0, 0, 0, 0, 0, 0)
+    end
 end
 
 entity.onEventUpdate = function(player, csid, option, npc)
     player:printToPlayer("Debug: Event update - csid: " .. csid .. ", option: " .. option)
     if csid == 11001 and option == 3 then
-        player:printToPlayer("Debug: Calling replenishFewell for option 3")
-        xi.synergy.replenishFewell(player, 1234) -- Coût fixé à 1234 gils
+        if player:hasKeyItem(xi.keyItem.SYNERGY_CRUCIBLE) then
+            player:printToPlayer("Debug: Player has SYNERGY_CRUCIBLE - Calling replenishFewell for option 3")
+            xi.synergy.replenishFewell(player, 1234) -- Coût fixé à 1234 gils
+        else
+            player:printToPlayer("Debug: Player does NOT have SYNERGY_CRUCIBLE - Calling buySynergyCrucible for option 3")
+            xi.synergy.buySynergyCrucible(player, 100)
+        end
     end
 end
 
