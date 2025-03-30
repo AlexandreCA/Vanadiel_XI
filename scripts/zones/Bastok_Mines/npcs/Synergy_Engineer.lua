3843affb67bd4f1ba592d503d3e2f81fcf2acb8b
Fox_mulder <alexandre.mate@protonmail.com>
2025-03-29 22:18:17 -0400

    Update Synergy_Engineer.lua

--- scripts/zones/Bastok_Mines/npcs/Synergy_Engineer.lua
+++ scripts/zones/Bastok_Mines/npcs/Synergy_Engineer.lua
@@ -10,17 +10,17 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-	player:startEvent(11001) -- Déclenche l'événement 11001
+    player:startEvent(11001)
 end
 
 entity.onEventUpdate = function(player, csid, option, npc)
 end
 
 entity.onEventFinish = function(player, csid, option, npc)
-    if csid == 11001 and option == 3 then -- Option 3 = "Acheter le Synergy Crucible"
+    if csid == 11001 and option == 3 then
         xi.synergy.buySynergyCrucible(player, 100)
     end
-    xi.synergy.onEventFinishEngineer(player, csid, option, npc) -- Garde la fonction existante
+    xi.synergy.onEventFinishEngineer(player, csid, option, npc)
 end
 
 return entity
