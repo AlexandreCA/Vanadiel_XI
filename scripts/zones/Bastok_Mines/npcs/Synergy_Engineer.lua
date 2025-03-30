d75b57091066c450ee599b05138dc2cdc73167c9
Fox_mulder <alexandre.mate@protonmail.com>
2025-03-30 00:02:35 -0400

    Update Synergy_Engineer.lua

--- scripts/zones/Bastok_Mines/npcs/Synergy_Engineer.lua
+++ scripts/zones/Bastok_Mines/npcs/Synergy_Engineer.lua
@@ -7,16 +7,14 @@
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    player:printToPlayer("Debug: Trade triggered")
 end
 
 entity.onTrigger = function(player, npc)
-    player:printToPlayer("Debug: Starting event 11001")
-    player:startEvent(11002)
+    player:startEvent(11001)
 end
 
 entity.onEventUpdate = function(player, csid, option, npc)
-    if csid == 11002 and option == 12 then
+    if csid == 11001 and option == 12 then
         xi.synergy.buySynergyCrucible(player, 100)
     end
 end
