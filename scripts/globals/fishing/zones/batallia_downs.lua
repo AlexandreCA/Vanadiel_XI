c130a408f3925339c691b980635913ba74ab26d1
Fox_mulder <alexandre.mate@protonmail.com>
2025-06-16 22:21:19 -0400

    update

---
+++ scripts/globals/fishing/zones/batallia_downs.lua
@@ -0,0 +1,17 @@
+-----------------------------------
+-- Batallia_Downs
+-----------------------------------
+return {
+    general = {
+        "Cobalt Jellyfish",
+        "Yellow Globe",
+        "Tiger Cod",
+        "Nosteau Herring",
+        "Cone Calamary",
+        "Bluetail",
+        "Black Sole",
+    },
+    south = {
+        "Silver Shark",
+    },
+},
---
+++ scripts/globals/fishing/zones/qufim_island.lua
@@ -0,0 +1,22 @@
+-----------------------------------
+-- Qufim_Island
+-----------------------------------
+return {
+    general = {
+        "Yellow Globe",
+        "Cone Calamary",
+        "Bluetail",
+    },
+    north sea = {
+        "Nosteau Herring",
+        "Gigant Squid",
+    },
+    southeast shore = {
+        "Three-eyed Fish",
+    },
+    southwest shore & ice pond  = {
+         "Black Sole",
+    },    
+}
+
+
